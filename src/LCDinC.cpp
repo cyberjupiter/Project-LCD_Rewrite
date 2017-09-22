4d206f4abf5ea56d7b1c128698167e53fbf0c373
Aiman Mazlan <aimanlpy@gmail.com>
2017-09-22 11:07:34 +0800

    Update LCDinC.cpp

--- src/LCDinC.cpp
+++ src/LCDinC.cpp
@@ -1,12 +1,12 @@
 //LCDinC.cpp
 
-#include "LCD_C.h"
+#include "LCDinC.h"
 
 uint8_t pins_control[2] = {RS, EN};
 uint8_t pins_data[8] = {D0, D1, D2, D3, D4, D5, D6, D7};
 
 
-void LCD_Cinit(void)
+void lcd_init(void)
 {
   for (uint8_t i = 0; i < 2; i++)
   {
@@ -25,72 +25,63 @@ void LCD_Cinit(void)
   //init LCD 2 lines, 5x7
   send_cmd(0b00111000);
   //send pulse
-  send_pulse(0);
-
   delay(300);
 
   //display on, cursor on
   send_cmd(0b00001110); 
   //send pulse
-  send_pulse(0);
   delay(30);
   
   //clear LCD
   send_cmd(0b00000001); 
   //send pulse
-  send_pulse(0);
   delay(30);
   
   //shift cursor right
   send_cmd(0b00000110);
   //send pulse
-  send_pulse(0);
   delay(30);
   
   //line 1, position 6
   send_cmd(0b10000110);
   //send pulse
-  send_pulse(0);
   delay(30);
   
   //write 'AIMAN, INIT OK'
-  lcd_write("AIMAN, INIT OK");
+  lcd_write("INIT OK");
 }
 
 void send_cmd(uint8_t wCmd)
 {
+  digitalWrite(RS, 0);
   for (uint8_t i = 0; i < 8; i++)
   {
     digitalWrite(pins_data[i], (wCmd >> i) & 0b0000001);
   }
+  send_pulse();
 }
 
-void send_pulse(uint8_t wInstruc)
+void send_data(uint8_t wData)
 {
-  if (wInstruc == 0)
+  digitalWrite(RS, 1);
+  for (uint8_t i = 0; i < 8; i++)
   {
-    digitalWrite(RS, 0);
-    digitalWrite(EN, 1);
-    delay(10);
-    digitalWrite(EN, 0);
+    digitalWrite(pins_data[i], (wData >> i) & 0b0000001);
   }
-  
-  else
-  {
-    digitalWrite(RS, 1);
-    digitalWrite(EN, 1);
-    delay(10);
-    digitalWrite(EN, 0);
-  }
+  send_pulse();
+}
+
+void send_pulse(void)
+{
+  digitalWrite(EN, 1);
+  delay(10);
+  digitalWrite(EN, 0);
 }
 
 void lcd_write(uint8_t *wChar)
 {
   for(uint8_t i = 0; wChar[i] != '\0'; i++)
     {
-      send_cmd(wChar[i]);
-      send_pulse(1);
+      send_data(wChar[i]);
     }
 }
-
-

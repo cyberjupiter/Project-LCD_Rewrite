06a2901cc5f92dad75ed4a44dd1457424c455769
Aiman Mazlan <aimanlpy@gmail.com>
2017-09-22 20:04:37 +0800

    Added new functions

--- src/LCDinC.cpp
+++ src/LCDinC.cpp
@@ -2,81 +2,115 @@
 
 #include "LCDinC.h"
 
-uint8_t pins_control[2] = {RS, EN};
-uint8_t pins_data[8] = {D0, D1, D2, D3, D4, D5, D6, D7};
+enum{
+	RS,
+	EN
+};
 
+uint8_t pins_control[2];
+uint8_t pins_data[8];
+
+void lcd_setPins(uint8_t rs, uint8_t en, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
+{
+	//instrucion register pins
+	pins_control[RS] = rs; 
+	pins_control[EN] = en;
+	
+	//data register pins
+	pins_data[0] = d0;
+	pins_data[1] = d1;
+	pins_data[2] = d2;
+	pins_data[3] = d3;
+	pins_data[4] = d4;
+	pins_data[5] = d5;
+	pins_data[6] = d6;
+	pins_data[7] = d7;
+}
 
 void lcd_init(void)
 {
-  for (uint8_t i = 0; i < 2; i++)
-  {
-    pinMode(pins_control[i], 1);
-    digitalWrite(pins_control[i], 0);
-  }
+	for (uint8_t i = 0; i < 2; i++)
+	{
+		pinMode(pins_control[i], 1);
+		digitalWrite(pins_control[i], 0);
+	}
 
-    for (uint8_t i = 0; i < 8; i++)
-  {
-    pinMode(pins_data[i], 1);
-    digitalWrite(pins_data[i], 0);
-  }
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		pinMode(pins_data[i], 1);
+		digitalWrite(pins_data[i], 0);
+	}
 
-  delay(500);
-  
-  //init LCD 2 lines, 5x7
-  send_cmd(0b00111000);
-  delay(300);
+	delay(500);
 
-  //display on, cursor on
-  send_cmd(0b00001110); 
-  delay(30);
-  
-  //clear LCD
-  send_cmd(0b00000001); 
-  delay(30);
-  
-  //shift cursor right
-  send_cmd(0b00000110);
-  delay(30);
-  
-  //line 1, position 6
-  send_cmd(0b10000110);
-  delay(30);
-  
-  //write "INIT OK"
-  lcd_write("INIT OK");
+	//init LCD 2 lines, 5x7
+	send_cmd(0b00111000);
+	delay(300);
+
+	//display on, cursor on
+	send_cmd(0b00001110); 
+	delay(30);
+
+	//clear LCD
+	send_cmd(0b00000001); 
+	delay(30);
+
+	//shift cursor right
+	send_cmd(0b00000110);
+	delay(30);
+
+	//line 1, position 6
+	send_cmd(0b10000110);
+	delay(30);
+
+	//write  "INIT OK"
+	lcd_write("INIT OK");
 }
 
-void send_cmd(uint8_t wCmd)
+void send_cmd(uint8_t pCmd)
 {
-  digitalWrite(RS, 0);
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    digitalWrite(pins_data[i], (wCmd >> i) & 0b0000001);
-  }
-  send_pulse();
+	digitalWrite(pins_control[RS], 0);
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		digitalWrite(pins_data[i], (pCmd >> i) & 0b0000001);
+	}
+	send_pulse();
 }
 
-void send_data(uint8_t wData)
+void send_data(uint8_t pData)
 {
-  digitalWrite(RS, 1);
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    digitalWrite(pins_data[i], (wData >> i) & 0b0000001);
-  }
-  send_pulse();
+	digitalWrite(pins_control[RS], 1);
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		digitalWrite(pins_data[i], (pData >> i) & 0b0000001);
+	}
+	send_pulse();
 }
 
 void send_pulse(void)
 {
-  digitalWrite(EN, 1);
-  delay(10);
-  digitalWrite(EN, 0);
+	digitalWrite(pins_control[EN], 1);
+	delay(10);
+	digitalWrite(pins_control[EN], 0);
 }
 
-void lcd_write(uint8_t *wChar)
+void lcd_write(uint8_t *pChar)
 {
-  for(uint8_t i = 0; wChar[i] != '\0'; i++)
-    {
-      send_data(wChar[i]);
-    }
+	for(uint8_t i = 0; pChar[i] != '\0'; i++)
+	{
+		send_data(wChar[i]);
+	}
+}
+
+void lcd_setCursor(uint8_t col, uint8_t row)
+{
+	if (row == 0)
+	{
+		send_cmd(0b10000000 + col);
+	}
+	
+	else  
+	{
+		send_cmd(0b11000000 + col);
+	}
 }

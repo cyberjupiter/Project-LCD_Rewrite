70266424f09288647588e0ed92c8963455b30124
Aiman Mazlan <aimanlpy@gmail.com>
2017-09-29 07:34:50 +0800

    Changed setPins to set8bit

--- src/LCDinC.h
+++ src/LCDinC.h
@@ -25,7 +25,7 @@ extern "C" {
 #include "Arduino.h"
 
 /* user available commands */
-void lcd_setPins(uint8_t rs, uint8_t en, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
+void lcd_set8bit(uint8_t rs, uint8_t en, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
 void lcd_init(void);
 void lcd_write(uint8_t *pChar);
 void lcd_setCursor(uint8_t col, uint8_t row);

65dc12bb2f9c23c9fec0725f140128c7c83b8b44
Aiman Mazlan <aimanlpy@gmail.com>
2017-09-29 18:46:48 +0800

    Update LCDinC.h

--- src/LCDinC.h
+++ src/LCDinC.h
@@ -7,20 +7,25 @@ extern "C" {
 #ifndef LCDinC_h
 #define LCDinC_h
 
-#define INIT        0b00110000
-#define CLEAR       0b00000001
+#define INIT          0b00110000
+#define CLEAR         0b00000001
   
-#define ROW0        0b10000000
-#define ROW1        0b11000000
-#define ROW2        0b10010100
-#define ROW3        0b11010100
+#define ROW0          0b10000000
+#define ROW1          0b11000000
+#define ROW2          0b10010100
+#define ROW3          0b11010100
 
-#define DISPLAYON   0b00001100
-#define DISPLAYOFF  0b00001000
-#define CURSORON    0b00001110
-#define CURSOROFF   0b00001100
-#define BLINKON     0b00001111
-#define BLINKOFF    0b00001110
+#define DISPLAYON     0b00001100
+#define DISPLAYOFF    0b00001000
+#define CURSORON      0b00001110
+#define CURSOROFF     0b00001100
+#define BLINKON       0b00001111
+#define BLINKOFF      0b00001110
+  
+#define ENTRYLEFT     0b00000100
+#define ENTRYRIGHT    0b00000110
+#define SHIFTALLLEFT  0b00000101
+#define SHIFTALLRIGHT 0b00000111
   
 #include "Arduino.h"
 
@@ -36,6 +41,12 @@ void lcd_cursorOn(void);
 void lcd_cursorOff(void);
 void lcd_blinkOn(void);
 void lcd_blinkOff(void);
+
+/* mid level commands, not neccessary but might be handy */
+void entryModeLeft(void);
+void entryModeRight(void);
+void shift_allLeft(void); //not working, will fix later
+void shift_allRight(void); //not working, will fix later
 
 /* low level commands */
 void send_cmd(uint8_t pCmd);

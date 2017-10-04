83c33b769cf339c327f7275da69bf6739dddf6eb
Aiman Mazlan <aimanlpy@gmail.com>
2017-10-04 20:56:15 +0800

    Added busy flag check

--- src/LCDinC.h
+++ src/LCDinC.h
@@ -33,7 +33,7 @@ extern "C" {
 #include "Arduino.h"
 
 /* user available commands */
-void lcd_set8bit(uint8_t rs, uint8_t en, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
+void lcd_set8bit(uint8_t rs, uint8_t rw, uint8_t en, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
 void lcd_init(void);
 void lcd_write(uint8_t *pChar);
 void lcd_setCursor(uint8_t col, uint8_t row);
@@ -52,6 +52,7 @@ void shift_allRight(void);
 void send_cmd(uint8_t pCmd);
 void send_data(uint8_t pData);
 void send_pulse(void);
+uint8_t read_busyflag(void);
 
 #endif
 
